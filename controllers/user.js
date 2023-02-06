94f67bb01abbde93f1becd8d0777cbf74dbbc211
[Vikash] <[vikashshukla160801@gmail.com]>
2023-02-06 14:54:36 +0530

    second time

--- controllers/user.js
+++ controllers/user.js
@@ -9,15 +9,19 @@ export const myProfile=(req,res,next)=>{
      });
 }
 
-export const logout=(req,res,next)=>{
-    req.session.destroy((err)=>{
-        if(err) return next(err)
-        res.clearCookie("connect.sid")
-        res.status(200).json({
-            message:"LOGGED OUT SUCCESSFULY",
-        });
+export const logout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) return next(err);
+    res.clearCookie("connect.sid", {
+      secure: process.env.NODE_ENV === "development" ? false : true,
+      httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      sameSite: process.env.NODE_ENV === "development" ? false : "none",
     });
-}
+    res.status(200).json({
+      message: "Logged Out",
+    });
+  });
+};
 
 export const getAdminUsers = asyncError(async (req, res, next) => {
     const users = await User.find({});
--- routes/user.js
+++ routes/user.js
@@ -8,10 +8,12 @@ router.get("/googlelogin",passport.authenticate("google",{
     scope:["profile"],
 }));
 
-router.get("/login",passport.authenticate("google"),
-(req,res,next)=>{
-    res.send("LOGGED IN");
-})
+router.get(
+    "/login",
+    passport.authenticate("google", {
+      successRedirect: process.env.FRONTEND_URL,
+    })
+  );
 
 router.get("/me",isAuthenticated,myProfile)
 
