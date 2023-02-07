1691c14898e34d4130311d993192fee96d36e2ad
[Vikash] <[vikashshukla160801@gmail.com]>
2023-02-07 22:07:52 +0530

    third commit

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMyOrders, placeOrderOnline, processOrder } from "../controllers/order.js";
+import { getMyOrders,  processOrder } from "../controllers/order.js";
 import { getOrderDetails } from "../controllers/order.js";
 import { getAdminOrders } from "../controllers/order.js";
 import { placeOrder } from "../controllers/order.js";
@@ -8,7 +8,7 @@ import { authorizeAdmin, isAuthenticated } from "../middlewares/Auth.js";
 const router=express.Router();
 
 router.post("/createorder",placeOrder);
-router.post("/createorderonline",placeOrderOnline)
+
 router.get("/myOrders",isAuthenticated,getMyOrders);
 router.get("/order/:id", isAuthenticated, getOrderDetails);
 router.get("/admin/orders", isAuthenticated,authorizeAdmin, getAdminOrders);
