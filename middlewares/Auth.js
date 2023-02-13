7fac22c672000054c13eb95340f3295f3b4a2395
[Vikash] <[vikashshukla160801@gmail.com]>
2023-02-13 11:16:59 +0530

    4

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,4 +1,4 @@
-import ErrorHandler from "../utils/ErrorHandler.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const isAuthenticated = (req, res, next) => {
   const token = req.cookies["connect.sid"];
