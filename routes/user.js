5dd0f3d0c8e9d7a1ddcc2b8e3ba444ff13d2f20c
[Vikash] <[vikashshukla160801@gmail.com]>
2023-02-20 16:11:41 +0530

    maan ja plz

--- routes/user.js
+++ routes/user.js
@@ -9,12 +9,11 @@ router.get("/googlelogin",passport.authenticate("google",{
 }));
 
 router.get(
-    "/login",
-    passport.authenticate("google", {
-      successRedirect: process.env.FRONTEND_URL,
-    })
-     );
-
+  "/login",
+  passport.authenticate("google", {
+    successRedirect: process.env.FRONTEND_URL,
+  })
+);
 router.get("/me",isAuthenticated,myProfile)
 
 router.get("/logout", logout)
